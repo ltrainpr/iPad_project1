c2663ee1e579585aa2b66dcac0053bbe5d3ec359
Lionel E. Ramos <lionelramos@hotmail.com>
2013-05-30 16:46:26 +0000

    First iPad Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Project1::Application.config.secret_token = '35d6b6f642834fa473172e3705142592f26f4bb344734965a7408d6b3867f89bd4d2aebf8a0f80b9f993d0510a34c3edc803b948d601ba2b7e231b3ab4741d06'
